476cea6d8d99f6d55067484484d7e2da5713595a
a-lives <xl3130504031@163.com>
2023-07-23 22:51:36 +0800

    add SettingPanel and rewrote the UIFrame class

---
+++ PC_Server/ServerSettingPanel.java
@@ -0,0 +1,15 @@
+
+
+/**
+ * 
+ * 
+ * 
+ */
+public class ServerSettingPanel extends UIFrame {
+    public ServerSettingPanel() throws Exception{
+        super("Rod-Server-Setting", SettingPanel.XPRO,SettingPanel.YPRO);
+        
+
+
+    }
+}
